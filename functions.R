8ebdf673b98949ba6d405733865bc3afa2dae172
李楚源 <lisa27chuyuan@gmail.com>
2018-10-31 22:06:26 +0100

    exo2 suite and exo3, exo4 first version

--- functions.R
+++ functions.R
@@ -6,6 +6,7 @@ difference_in_medians <- function(d, var, grouping_var, group1, group2) {
   return(result)
 }
 
+
 randomize <- function(d, var) {
   d[[var]] <- sample(d[[var]])
   return(d)
@@ -33,6 +34,19 @@ p_value_mc_right <- function(simulated, observed) {
 p_value_mc_left <- function(simulated, observed) {
   return(mean(simulated <= observed))
 }
+
+
+## Exo 3
+permutation_t_test <- function(d, value, grouping_var, group1, group2, N_SAMPLES=9999){
+  statistics <- rep(NA, N_SAMPLES)
+  for (i in 1:N_SAMPLES){
+    d[[grouping_var]] <- sample(d[[grouping_var]])
+    # run t-test for permutation groups
+    statistics[i] <- t.test(dplyr::filter(d, get(grouping_var) == group1)$value, dplyr::filter(d, get(grouping_var) == group2)$value)$statistic
+  } 
+  return (statistics)
+}
+
 
 
 
